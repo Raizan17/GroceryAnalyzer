fbf13e701a7493d670e6677d118a1965c905fcbb
Raizan17 <raizan.lee17@gmail.com>
2023-11-26 20:08:50 -0700

    Created Table Structure for Items
    Created Table Strucuture for items from different Departments. Made proper use of css and divs to form expandable placeholders for data queried from database.

--- client/src/pages/Meat.jsx
+++ client/src/pages/Meat.jsx
@@ -1,11 +1,719 @@
-import React from 'react'
+import React from 'react';
+import '../css/bakery.css'; // Import your CSS file
+import Apple from "../img/apple.jpg";
+import Banana from "../img/banana.jpg";
+import Walmart from "../img/walmart.png";
+import Freshco from "../img/freshco.png";
+import Real from "../img/real.png";
+import Safeway from "../img/safeway.png";
 
 const Meat = () => {
   return (
     <div>
-      
+
+      {/* APPLE */}
+
+    <div className="centered-box">
+      {/* First Row: Big Image */}
+      <div className="rowA">
+        <img src={Apple} alt="" className="big-image" />
+      </div>
+
+      {/* Second Row: Text */}
+      <div className="rowA">
+        <p>APPLE</p>
+      </div>
+
+     {/* Columns*/}
+     
+    <div className="five-column-container">
+      {/* Column 1 */}
+      <div className="column">
+         {/* Row 0 */}
+         <div className="row" > 
+         <div className="child">
+          <p>Downtown</p>
+          </div>
+        </div>
+        {/* Row 1 */}
+        <div className="row">
+          <div className="column">
+            <img src={Walmart} alt="" className="row-image" />
+          </div>
+          <div className="column">
+            <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+        {/* Row 2 */}
+        <div className="row">
+          <div className="column">
+            <img src={Freshco} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 3 */}
+         <div className="row">
+          <div className="column">
+            <img src={Real} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 4 */}
+         <div className="row">
+          <div className="column">
+            <img src={Safeway} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+     {/* Column 2 */}
+     <div className="column">
+         {/* Row 0 */}
+         <div className="row centerd" > 
+         <div className="child">
+          <p>North West</p>
+          </div>
+        </div>
+        {/* Row 1 */}
+        <div className="row">
+          <div className="column">
+            <img src={Walmart} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+        {/* Row 2 */}
+        <div className="row">
+          <div className="column">
+            <img src={Freshco} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 3 */}
+         <div className="row">
+          <div className="column">
+            <img src={Real} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 4 */}
+         <div className="row">
+          <div className="column">
+            <img src={Safeway} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
+     {/* Column 3 */}
+     <div className="column">
+         {/* Row 0 */}
+         <div className="row centerd" > 
+         <div className="child">
+          <p>North East</p>
+          </div>
+        </div>
+        {/* Row 1 */}
+        <div className="row">
+          <div className="column">
+            <img src={Walmart} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+        {/* Row 2 */}
+        <div className="row">
+          <div className="column">
+            <img src={Freshco} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 3 */}
+         <div className="row">
+          <div className="column">
+            <img src={Real} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 4 */}
+         <div className="row">
+          <div className="column">
+            <img src={Safeway} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
+     {/* Column 4 */}
+     <div className="column">
+         {/* Row 0 */}
+         <div className="row centerd" > 
+         <div className="child">
+          <p>South West</p>
+          </div>
+        </div>
+        {/* Row 1 */}
+        <div className="row">
+          <div className="column">
+            <img src={Walmart} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+        {/* Row 2 */}
+        <div className="row">
+          <div className="column">
+            <img src={Freshco} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 3 */}
+         <div className="row">
+          <div className="column">
+            <img src={Real} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 4 */}
+         <div className="row">
+          <div className="column">
+            <img src={Safeway} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
+     {/* Column 5 */}
+     <div className="column">
+         {/* Row 0 */}
+         <div className="row centerd" > 
+         <div className="child">
+          <p>South East</p>
+          </div>
+        </div>
+        {/* Row 1 */}
+        <div className="row">
+          <div className="column">
+            <img src={Walmart} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+        {/* Row 2 */}
+        <div className="row">
+          <div className="column">
+            <img src={Freshco} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 3 */}
+         <div className="row">
+          <div className="column">
+            <img src={Real} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 4 */}
+         <div className="row">
+          <div className="column">
+            <img src={Safeway} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+      </div>
+
     </div>
-  )
+
+  </div>
+
+    {/* BANANA*/}
+  
+  
+    <div className="centered-box">
+      {/* First Row: Big Image */}
+      <div className="rowA">
+        <img src={Banana} alt="" className="big-image" />
+      </div>
+
+      {/* Second Row: Text */}
+      <div className="rowA">
+        <p>BANANA</p>
+      </div>
+
+     {/* Columns*/}
+     
+    <div className="five-column-container">
+      {/* Column 1 */}
+      <div className="column">
+         {/* Row 0 */}
+         <div className="row" > 
+         <div className="child">
+          <p>Downtown</p>
+          </div>
+        </div>
+        {/* Row 1 */}
+        <div className="row">
+          <div className="column">
+            <img src={Walmart} alt="" className="row-image" />
+          </div>
+          <div className="column">
+            <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+        {/* Row 2 */}
+        <div className="row">
+          <div className="column">
+            <img src={Freshco} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 3 */}
+         <div className="row">
+          <div className="column">
+            <img src={Real} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 4 */}
+         <div className="row">
+          <div className="column">
+            <img src={Safeway} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+     {/* Column 2 */}
+     <div className="column">
+         {/* Row 0 */}
+         <div className="row centerd" > 
+         <div className="child">
+          <p>North West</p>
+          </div>
+        </div>
+        {/* Row 1 */}
+        <div className="row">
+          <div className="column">
+            <img src={Walmart} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+        {/* Row 2 */}
+        <div className="row">
+          <div className="column">
+            <img src={Freshco} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 3 */}
+         <div className="row">
+          <div className="column">
+            <img src={Real} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 4 */}
+         <div className="row">
+          <div className="column">
+            <img src={Safeway} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
+     {/* Column 3 */}
+     <div className="column">
+         {/* Row 0 */}
+         <div className="row centerd" > 
+         <div className="child">
+          <p>North East</p>
+          </div>
+        </div>
+        {/* Row 1 */}
+        <div className="row">
+          <div className="column">
+            <img src={Walmart} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+        {/* Row 2 */}
+        <div className="row">
+          <div className="column">
+            <img src={Freshco} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 3 */}
+         <div className="row">
+          <div className="column">
+            <img src={Real} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 4 */}
+         <div className="row">
+          <div className="column">
+            <img src={Safeway} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
+     {/* Column 4 */}
+     <div className="column">
+         {/* Row 0 */}
+         <div className="row centerd" > 
+         <div className="child">
+          <p>South West</p>
+          </div>
+        </div>
+        {/* Row 1 */}
+        <div className="row">
+          <div className="column">
+            <img src={Walmart} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+        {/* Row 2 */}
+        <div className="row">
+          <div className="column">
+            <img src={Freshco} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 3 */}
+         <div className="row">
+          <div className="column">
+            <img src={Real} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 4 */}
+         <div className="row">
+          <div className="column">
+            <img src={Safeway} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
+     {/* Column 5 */}
+     <div className="column">
+         {/* Row 0 */}
+         <div className="row centerd" > 
+         <div className="child">
+          <p>South East</p>
+          </div>
+        </div>
+        {/* Row 1 */}
+        <div className="row">
+          <div className="column">
+            <img src={Walmart} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+        {/* Row 2 */}
+        <div className="row">
+          <div className="column">
+            <img src={Freshco} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 3 */}
+         <div className="row">
+          <div className="column">
+            <img src={Real} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+         {/* Row 4 */}
+         <div className="row">
+          <div className="column">
+            <img src={Safeway} alt="" className="row-image" />
+          </div>
+          <div className="column">
+          <div className='write'>
+            Price: 35 
+            Quantity: 20
+            Calorie: 4000.00
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  
+
+  </div>
+    
+    
+  );
 }
 
 export default Meat;
